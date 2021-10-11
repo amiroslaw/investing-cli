78552f6b01cef8bbbc479a6bcf3545013605dcf5
amiroslaw <arek47@gmail.com>
2021-10-12 01:06:33 +0200

    tree model

--- src/main/java/ovh/miroslaw/investing/portfolio/PortfolioReader.java
+++ src/main/java/ovh/miroslaw/investing/portfolio/PortfolioReader.java
@@ -1,14 +1,19 @@
 package ovh.miroslaw.investing.portfolio;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ovh.miroslaw.investing.RetrievingDataException;
 import ovh.miroslaw.investing.RetrievingDataException.ErrorCode;
+import ovh.miroslaw.investing.model.AlertCondition;
+import ovh.miroslaw.investing.model.AssetType;
 import ovh.miroslaw.investing.model.Portfolio;
+import ovh.miroslaw.investing.model.Portfolio.Alert;
+import ovh.miroslaw.investing.model.Portfolio.Holding;
 
 import java.io.File;
-import java.io.IOException;
+import java.math.BigDecimal;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -24,12 +29,57 @@ public class PortfolioReader {
     public static List<Portfolio> getPortfolio(File portfolioConfig) throws RetrievingDataException {
         try {
             ObjectMapper mapper = new ObjectMapper();
-            return mapper.readValue(portfolioConfig, new TypeReference<List<Portfolio>>() {
-            });
-        } catch (IOException e) {
+            JsonNode assets = mapper.readTree(portfolioConfig);
+            List<Portfolio> portfolio = new ArrayList<>();
+            for (JsonNode asset : assets) {
+                final String assetName = asset.get("assetName").asText();
+                final String assetSymbol = asset.get("assetSymbol").asText();
+
+                final AssetType assetType = AssetType.valueOf(asset.get("type").asText("GPW"));
+                final JsonNode alertsNode = asset.get("alerts");
+                final JsonNode holdingsNode = asset.get("holdings");
+                final ArrayList<Portfolio.Alert> alerts = getAlerts(alertsNode);
+                final ArrayList<Portfolio.Holding> holdings = getHoldings(holdingsNode);
+                portfolio.add(new Portfolio(assetName, assetSymbol, assetType, holdings, alerts));
+            }
+            return portfolio;
+        } catch (Exception e) {
             final String errorMsg = "Error with reading portfolio file from: " + portfolioConfig.getAbsolutePath();
             throw new RetrievingDataException(errorMsg, ErrorCode.SOFTWARE);
         }
+    }
+
+    private static ArrayList<Holding> getHoldings(JsonNode holdingsNode) {
+        final ArrayList<Portfolio.Holding> holdings = new ArrayList<>();
+        if (holdingsNode == null) {
+            return new ArrayList<>();
+        }
+        for (JsonNode holding : holdingsNode) {
+            BigDecimal buyPrice = null;
+            if (holding.get("buyPrice") != null) {
+                buyPrice = BigDecimal.valueOf(holding.get("buyPrice").asDouble());
+            }
+            BigDecimal sellPrice = null;
+            if (holding.get("sellPrice") != null) {
+                sellPrice = BigDecimal.valueOf(holding.get("sellPrice").asDouble());
+            }
+            final double amount = holding.get("amount").asDouble();
+            holdings.add(new Holding(buyPrice, sellPrice, amount));
+        }
+        return holdings;
+    }
+
+    private static ArrayList<Portfolio.Alert> getAlerts(JsonNode alertsNode) {
+        if (alertsNode == null) {
+            return new ArrayList<>();
+        }
+        final ArrayList<Portfolio.Alert> alerts = new ArrayList<>();
+        for (JsonNode alert : alertsNode) {
+            final AlertCondition alertCondition = AlertCondition.valueOf(alert.get("alertCondition").asText("ABOVE"));
+            final BigDecimal price = BigDecimal.valueOf(alert.get("price").asDouble());
+            alerts.add(new Alert(price, alertCondition));
+        }
+        return alerts;
     }
 
     public static Path getConfigPath() {
